11f6601ef368ecee92f0344ab241beb1951d2c20
Roman <user@email.com>
2023-12-03 21:21:47 +0500

    added task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Написать программу, которая заполняет массив размерности n x n по диагонали
+
+
+int Zapros(string message) {
+    Console.WriteLine(message);
+    int num = System.Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+int[,] GetDoubleMassFromDiagonal(int rows, int columns) {
+    int[,] arr = new int[rows, columns];
+    int temp = 1;
+        for (int i = 1 - rows; i <= rows - 1; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                int k = j - i;
+                if (k < 0 || k >= rows)
+                    continue;
+                    arr[j, k] = temp++;
+                }
+            }
+    return arr;
+}
+
+void PrintArr(int[,] arr) {
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int ROWS = Zapros("введите количество строк");
+int COLUMNS = Zapros("введите количество столбцов");
+int[,] DiagonalArray = GetDoubleMassFromDiagonal(ROWS,COLUMNS);
+PrintArr(DiagonalArray);
